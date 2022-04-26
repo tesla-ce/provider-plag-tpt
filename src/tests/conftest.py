552dec0c20cc56d46d0f42eacdfba323a84f47d2
Roger <roger@rcod.net>
2022-04-26 09:28:40 +0200

    first commit

---
+++ src/tests/conftest.py
@@ -0,0 +1,149 @@
+#  Copyright (c) 2020 Roger Muñoz Bernaus
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU Affero General Public License as
+#      published by the Free Software Foundation, either version 3 of the
+#      License, or (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU Affero General Public License for more details.
+#
+#      You should have received a copy of the GNU Affero General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+""" Test fixtures module """
+from logging import getLogger
+import os
+import pytest
+
+
+@pytest.fixture
+def tesla_ce_provider_conf():
+    '''
+    TeSLACE provider conf fixture
+    :return:
+    '''
+    return {
+        'provider_class': 'tpt.TPTProvider',
+        'provider_desc_file': None,
+        'instrument': None,
+        'info': None
+    }
+
+
+@pytest.fixture
+def urkund_provider(tesla_ce_base_provider):
+    '''
+    Urkund Provider Fixture
+    :param tesla_ce_base_provider:
+    :return:
+    '''
+    from tpt import TPTProvider
+    assert isinstance(tesla_ce_base_provider, TPTProvider)
+
+    logger = getLogger('tpt Tests')
+    tesla_ce_base_provider.set_logger(logger.info)
+
+    tesla_ce_base_provider.set_credential('SECRET', os.getenv('SECRET', None))
+
+    return tesla_ce_base_provider
+
+
+@pytest.fixture
+def mock_urkund_lib_all_ok(mocker, urkund_provider):
+    """
+    Mocker urkund lib all methods ok
+    :param mocker:
+    :param urkund_provider:
+    :return:
+    """
+    mock_module = {
+        'Id': 1,
+        'Name': 'TeSLA CE Institution Test',
+        'Suffix': '.uoc@analysis.urkund.com',
+        'DateCreated': '12/18/2021 11:54:00 AM',
+        'Organizations': [
+            {'Id': 1, 'Name': "TeSLA CE Test", 'SubOrganizations': []}
+        ],
+        'MOCKED': True
+    }
+    mocker.patch('tpt.urkund_lib.units.Units.get', return_value=mock_module)
+
+    mock_module = {
+        'Id': 414194,
+        'AnalysisAddress': 'rmunozber.uoc@analysis.urkund.com',
+        'EmailAddress': 'rmunozber@uoc.edu',
+        'FullName': 'Roger John Doe',
+        'Language': 'EN ',
+        'UnitId': 1,
+        'Organization': {'Id': 1, 'Name': 'Estudis', 'SubOrganizations': None},
+        'SubOrganization': None,
+        'MOCKED': True
+    }
+
+    mocker.patch('urkund.urkund_lib.receivers.Receivers.get', return_value=mock_module)
+
+    mock_module = {
+        'Id': 72565518,
+        'ExternalId': '1610712121.915711_0',
+        'Filename': 'lorem.txt',
+        'MimeType': 'text/plain',
+        'Timestamp': '2021-01-15T12:03:06',
+        'Status': {'State': 'Submitted', 'ErrorCode': 0, 'Message': 'The document has been sent to URKUND.'},
+        'Document': None,
+        'Report': None,
+        'Subject': '',
+        'Message': '',
+        'Anonymous': None,
+        'AutoDeleteDocument': False,
+        'MOCKED': True
+    }
+
+    mocker.patch('urkund.urkund_lib.submissions.Submissions.send', return_value=mock_module)
+
+    mock_module = [{
+        'Id': 72565518,
+        'ExternalId': '1610712121.915711_0',
+        'Filename': 'lorem.txt',
+        'MimeType': 'text/plain',
+        'Timestamp': '2021-01-15T12:03:06',
+        'Status': {
+            'State': 'Analyzed',
+            'ErrorCode': 0,
+            'Message': 'The document has been analyzed.'
+        },
+        'Document': {
+            'Id': 92305055,
+            'Date': '2021-01-15T12:03:00',
+            'DownloadUrl': 'https://secure.urkund.com/archive/download?c1=92305055&amp;c2=676372&amp;c3=525345',
+            'OptOutInfo': {
+                'Url': 'https://secure.urkund.com/account/document/exemptionstatus/92305055-676372-525345',
+                'Message': 'As the author of the document you have submitted, it is within your right to hide '
+                           'the text content from being viewed by other educational organisations. Note that by '
+                           'hiding the text content, you will not delete the document. The document will remain '
+                           'stored and checked against internal and external sources and forwarded to your tutor '
+                           'along with a plagiarism report. The text content might also be visible within your '
+                           'educational organisation, depending on their preferred settings. However, the text '
+                           'content of the document will not be visible for any user outside your educational '
+                           'organisation.\n\nBear in mind that by hiding your document, it will not be protected '
+                           'against plagiarism. This means that an unauthorised person will be able to make use '
+                           'of your work in the future. If you want to protect your copyright, you should leave '
+                           'it unhidden.\n\nTo hide the text content of your document, click the below link:'}
+        },
+        'Report': {
+            'Id': 88349077,
+            'Significance': 5.86,
+            'MatchCount': 833,
+            'SourceCount': 75,
+            'ReportUrl': 'https://secure.urkund.com/view/88349077-508223-368549',
+            'Warnings': []
+        },
+        'Subject': '',
+        'Message': '',
+        'Anonymous': None,
+        'AutoDeleteDocument': False,
+        'MOCKED': True
+    }]
+
+    mocker.patch('urkund.urkund_lib.submissions.Submissions.result', return_value=mock_module)
---
+++ src/tpt/provider/tpt.py
@@ -0,0 +1,172 @@
+#  Copyright (c) 2020 Roger Muñoz Bernaus
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU Affero General Public License as
+#      published by the Free Software Foundation, either version 3 of the
+#      License, or (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU Affero General Public License for more details.
+#
+#      You should have received a copy of the GNU Affero General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+""" TeSLA CE TPT Plagiarism module """
+import os
+import uuid
+
+from tesla_ce_provider import BaseProvider, result, message
+from tesla_ce_provider.provider.audit.tp import PlagiarismAudit
+from . import utils
+
+
+class TPTProvider(BaseProvider):
+    """
+        TeSLA TPT implementation
+    """
+    accepted_mimetypes = [
+        'application/zip',
+        'application/gzip',
+        'application/x-tar',
+        'application/x-bzip2',
+        'application/x-7z-compressed',
+        'application/x-rar-compressed',
+        'application/x-lzma',
+        'application/msword',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'application/vnd.sun.xml.writer',
+        'application/vnd.ms-powerpoint',
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'application/pdf',
+        'text/plain',
+        'application/rtf',
+        'text/html',
+        'text/html',
+        'application/vnd.ms-works',
+        'application/vnd.oasis.opendocument.text'
+    ]
+
+    _required_credentials = ['SECRET']
+
+    config = {
+        'domain': None,
+        'compression_recursive_extract_level': 3,
+        'timeout_retries': 0,
+        'max_timeout_retries': 3,
+        'timeout_between_retries': 60,
+        'countdown_multiplier': 2,
+        'countdown_initial': 5
+    }
+
+    def set_options(self, options):
+        """
+            Set options for the provider
+            :param options: Provider options following provider options_scheme definition
+            :type options: dict
+        """
+        if options is not None:
+            permitted_options = self.config.keys()
+
+            for permitted_option in permitted_options:
+                if permitted_option in options:
+                    self.config[permitted_option] = options[permitted_option]
+
+    def verify(self, request, model):
+        """
+            Verify a learner request
+            :param request: Verification request
+            :type request: Request
+            :param model: Provider model
+            :type model: dict
+            :return: Verification result
+            :rtype: tesla_provider.VerificationResult
+        """
+
+        # Check provided input
+        sample_check = utils.check_sample_file(request, self.config['compression_recursive_extract_level'],
+                                               self.accepted_mimetypes)
+
+        if not sample_check['valid']:
+            return result.VerificationResult(False, error_message=sample_check['msg'],
+                                             message_code=sample_check['code'])
+
+        # todo: check configuration activity
+
+
+        # sample is valid proceed to sent to urkund
+        idx = 0
+        info = {
+            "learner_id": request.learner_id,
+            "request_id": request.request_id,
+            "external_ids": [],
+            "processed_external_ids": {
+                "errors": [],
+                "corrects": []
+            },
+            "errors": [],
+            "countdown": self.config['countdown_initial'],
+            "total_files": len(sample_check['tree_file'])
+        }
+
+        for t_file in sample_check['tree_file']:
+            if t_file['status'] == 'ACCEPTED':
+                file = {
+                    "filename": t_file['filename'],
+                    "mimetype": t_file['mimetype'],
+                    "content": t_file['content']
+                }
+                external_id = "{}__{}".format(request.request_id, idx)
+
+                try:
+                    # send to TPT
+                    # TODO: where is data?
+                    data = {
+                        "activity": {
+                            "vle_id": "1",
+                            "activity_type": "assign",
+                            "activity_id": "1"
+                        },
+                        "sample_data": get_sample('2.txt', 'plain/text'),
+                        "learner_id": request.learner_id,
+                        "evaluation_id": external_id
+                    }
+                    response = requests.post('{}/api/v1/evaluation/new/'.format(base_url), json=data)
+                    # good response
+                    # {"request_id":1,"status_code":"0"}
+
+                    if int(response['status_code']) == 0:
+                        aux = {
+                            "external_id": external_id,
+                            "filename": t_file['filename']
+                        }
+                        info['external_ids'].append(aux)
+
+                    else:
+                        aux = {
+                            "code": message.provider.Provider.PROVIDER_INVALID_SAMPLE_DATA.value,
+                            "filename": t_file['filename'],
+                            "tpt_code": response['status_code']
+                        }
+                        info['processed_external_ids']['errors'].append(aux)
+
+                    idx += 1
+                except Timeout:
+                    self.config['timeout_retries'] += 1
+                    if self.config['timeout_retries'] >= self.config['max_timeout_retries']:
+                        aux = {
+                            "code": message.provider.Provider.PROVIDER_EXTERNAL_SERVICE_TIMEOUT.value,
+                            "filename": t_file['filename'],
+                            "tpt_code": None
+                        }
+                        info['processed_external_ids']['errors'].append(aux)
+                    else:
+                        return self.verify(request, model)
+
+        # check if there is any sent document valid to check in the future
+        if len(info['external_ids']) == 0:
+            return result.VerificationResult(False, message_code=message.provider.Provider.PROVIDER_INVALID_SAMPLE_DATA.value)
+        elif len(info['processed_external_ids']['errors']) > 0 and len(info['external_ids']) > 0:
+            return result.VerificationResult(True, message_code=message_code=message.provider.Provider.PROVIDER_INVALID_PARTIAL_SAMPLE_DATA.value)
+
+        return result.VerificationResult(True)
